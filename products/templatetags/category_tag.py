66da06922d99751f29317122ba2a78b844815efa
Vitalii <kuzvitalii@gmail.com>
2020-09-15 13:11:15 +0300

    edit category menu filter

--- products/templatetags/category_tag.py
+++ products/templatetags/category_tag.py
@@ -7,10 +7,22 @@ register = template.Library()
 
 @register.inclusion_tag('category_menu.html')
 def category_menu(req):
-    new_cat = list()
+    def create_child(children):
+        child_list = []
+        for ch in children:
+            child_list.append({
+                'name': ch.name,
+                'url': ch.get_absolute_url(),
+                'child': True if len(ch.children.all()) > 0 else False,
+                'parent': True if len(ch.parents.all()) > 0 else False,
+                'children': create_child(ch.children.all())})
+        return child_list
+
+    format_ctr = list()
     for c in Category.objects.all():
-        if len(c.children.all()) > 0:
-            new_cat.append({'name': c.name, 'url': c.get_absolute_url(), 'child': True, 'parent': True if len(c.parents.all()) > 0 else False, 'children': [{'name': ch.name, 'url': ch.get_absolute_url(), 'child': True if len(ch.children.all()) > 0 else False, 'children': []} for ch in c.children.all()]})
-        else:
-            new_cat.append({'name': c.name, 'url': c.get_absolute_url(), 'child': False, 'parent': True if len(c.parents.all()) > 0 else False, 'children': []})
-    return {'categories': new_cat, 'request': req}
+        format_ctr.append(
+            {'name': c.name, 'url': c.get_absolute_url(),
+             'child': True if len(c.children.all()) > 0 else False,
+             'parent': True if len(c.parents.all()) > 0 else False,
+             'children': create_child(c.children.all())})
+    return {'categories': format_ctr, 'request': req}
